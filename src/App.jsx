9ae69ec4624732f8f0e4477971bf1263eb28d642
Ryan Bennek <rbennek.rb@gmail.com>
2024-01-26 11:08:36 -0600

    implement serverside

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,34 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
 
   return (
-    <>
-      <div></div>
-    </>
+    <main>
+      <h1>Animal Farm</h1>
+
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => console.log(e.target.value)}
+      />
+
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal}></Animal>
+        ))}
+
+        {animals.length === 0 && 'No animals found'}
+      </ul>
+    </main>
+  );
+}
+
+function Animal({ type, name, age }) {
+  return (
+    <li>
+      <strong>{type}</strong> {name} ({age} years old)
+    </li>
   );
 }
 
