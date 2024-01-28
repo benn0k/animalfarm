75b0f3908d1335b15773efaa21f7c3ade7554468
Ryan Bennek <rbennek.rb@gmail.com>
2024-01-28 10:19:56 -0600

    Clean up Server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,14 @@
-import cors from "cors";
-import express from "express";
-
-// ? weird, i've never seen this before
-import { createRequire } from "module";
+import cors from 'cors';
+import express from 'express';
+import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 // Load Chance
 // TODO use const
-var Chance = require("chance");
+const Chance = require('chance');
 
 // Instantiate Chance so it can be used
-// TODO use const
-var chance = new Chance();
+const chance = new Chance();
 
 //init Express app
 const app = express();
@@ -29,18 +26,19 @@ const animals = [...Array(250).keys()].map((id) => {
 });
 
 //Endpoint to search for animals
-// TODO best practice is "/" here for the index route
-app.get("", (req, res) => {
+app.get('/', (req, res) => {
   //filter results by query
-  const q = req.query.q?.toLowerCase() || ""; // * GREAT use of optional chaining here
+  const q = req.query.q?.toLowerCase() || '';
 
   // TODO if you wanted to, you could send results back early if the query is empty, maybe an error?
 
   // ? imagine if you had a million animals, this would be slow just to filter and find nothing
-  const results = animals.filter((animal) => animal.type.toLowerCase().includes(q));
+  const results = animals.filter((animal) =>
+    animal.type.toLowerCase().includes(q)
+  );
 
   res.send(results);
 });
 
-// TODO discuss .env vars for PORT
-app.listen(8080, () => console.log("Listening on port http://localhost:8080"));
+// TODO move to .env
+app.listen(8080, () => console.log('Listening on port http://localhost:8080'));
